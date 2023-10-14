2e7cbffd473faddc36498fd862f940b57649e838
khiemhuynh95 <khiemhuynh952@gmail.com>
2023-10-14 00:21:06 -0500

    remove password

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://khiemhuynh952:HFp2lGMFzvtJllwM@cluster0.ovizq1e.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://khiemhuynh952:<password>@cluster0.ovizq1e.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function connectToMongoDB() {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB!");
-    return client;
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    throw error;
-  }
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+        return client;
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
 }
 
 module.exports = { connectToMongoDB };
